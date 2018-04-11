777801c47b4e03619bdee9ade924c028b30739b3
ciamiz <luca@ramundo.info>
2018-04-11 12:49:06 +0200

    Draft options handling

---
+++ src/options.js
@@ -0,0 +1,65 @@
+const pipe = require("popipe")
+
+const init = yargs =>
+  yargs.detectLocale(false)
+
+const usage = yargs =>
+  yargs.usage("Usage: $0 [options]")
+
+const help = yargs =>
+  yargs
+    .help("help")
+    .alias("help", "h")
+
+const examples = yargs => yargs
+
+const options = yargs =>
+  yargs.options({
+    "branch": {
+      alias: "b",
+      describe: "Filter commits by this branch"
+    },
+    "users": {
+      alias: "u",
+      describe: "Filter commits by these users",
+      array: true
+    },
+    "from": {
+      alias: "f",
+      describe: "Filter commits from this date"
+    },
+    "to": {
+      alias: "t",
+      describe: "Filter commits until this date"
+    },
+    "continuity": {
+      alias: "c",
+      describe: "Hours between commits to be considered as worked",
+      default: 1
+    },
+    "prestart": {
+      alias: "ps",
+      describe: "Hours before the first commit per day to be considered as worked",
+      default: 1
+    }
+  })
+
+const coercions = yargs => yargs
+
+const demands = yargs => yargs
+
+const parse = yargs =>
+  pipe(
+    yargs,
+    init,
+    usage,
+    help,
+    examples,
+    options,
+    coercions,
+    demands
+  )
+
+module.exports = {
+  parse
+}
