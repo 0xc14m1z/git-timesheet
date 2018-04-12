72934b3bbab3d6324b83a09fd16dd33d67378b95
ciamiz <luca@ramundo.info>
2018-04-12 17:17:39 +0200

    Add commit data structure creation method and relative specs

---
+++ specs/git/commit.js
@@ -0,0 +1,24 @@
+const expect = require("chai").expect
+const rewire = require("rewire")
+
+const SOURCE_FILE = "../../lib/git/commit"
+const currentModule = rewire(SOURCE_FILE)
+
+const shellModule = rewire("../../lib/shell")
+
+describe("Commit", () => {
+
+  const Commit = currentModule.__get__("Commit")
+
+  it("should return a Commit data structure from raw date and author", () => {
+    const rawDate = "2018-01-08 16:44:24 +0100d"
+    const author = "first@user.com"
+
+    const expectation =
+      { year: "2018", month: "01", day: "08", hour: "16", minute: "44", second: "24", author: "first@user.com" }
+
+    const result = Commit(rawDate, author)
+    expect(result).to.deep.equal(expectation)
+  })
+
+})
