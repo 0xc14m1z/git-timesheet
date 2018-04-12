759b16cc785227fb0f6eb0395d212847fd7500af
ciamiz <luca@ramundo.info>
2018-04-12 18:55:18 +0200

    Implement filters and refine specs

--- specs/filters.js
+++ specs/filters.js
@@ -7,32 +7,17 @@ describe("Filters", () => {
 
   describe("by", () => {
 
-    it("should filter an array of commit data structures by an author", () => {
-      const commits = [
-        { year: "2018", month: "01", day: "08", hour: "16", minute: "44", second: "24", author: "first@user.com" },
-        { year: "2017", month: "02", day: "23", hour: "17", minute: "03", second: "02", author: "second@user.com" },
-        { year: "2018", month: "03", day: "15", hour: "16", minute: "42", second: "00", author: "third@user.com" }
-      ]
-
-      const expectation = [
-        { year: "2017", month: "02", day: "23", hour: "17", minute: "03", second: "02", author: "second@user.com" }
-      ]
-
-      const result = Filters.by("second@user.com", commits)
-      expect(result).to.deep.equal(expectation)
-    })
-
     it("should filter an array of commit data structures by an array of authors", () => {
       const authors = ["second@user.com", "third@user.com"]
       const commits = [
-        { year: "2018", month: "01", day: "08", hour: "16", minute: "44", second: "24", author: "first@user.com" },
-        { year: "2017", month: "02", day: "23", hour: "17", minute: "03", second: "02", author: "second@user.com" },
-        { year: "2018", month: "03", day: "15", hour: "16", minute: "42", second: "00", author: "third@user.com" }
+        { date: "2018-01-08", hour: "16", author: "first@user.com" },
+        { date: "2017-02-23", hour: "17", author: "second@user.com" },
+        { date: "2018-03-15", hour: "16", author: "third@user.com" }
       ]
 
       const expectation = [
-        { year: "2017", month: "02", day: "23", hour: "17", minute: "03", second: "02", author: "second@user.com" },
-        { year: "2018", month: "03", day: "15", hour: "16", minute: "42", second: "00", author: "third@user.com" }
+        { date: "2017-02-23", hour: "17", author: "second@user.com" },
+        { date: "2018-03-15", hour: "16", author: "third@user.com" }
       ]
 
       const result = Filters.by(authors, commits)
@@ -45,14 +30,14 @@ describe("Filters", () => {
 
     it("should filter an array of commit data structures from a certain date", () => {
       const commits = [
-        { year: "2018", month: "01", day: "08", hour: "16", minute: "44", second: "24", author: "first@user.com" },
-        { year: "2017", month: "02", day: "23", hour: "17", minute: "03", second: "02", author: "second@user.com" },
-        { year: "2018", month: "03", day: "15", hour: "16", minute: "42", second: "00", author: "third@user.com" }
+        { date: "2018-01-08", hour: "16", author: "first@user.com" },
+        { date: "2017-02-23", hour: "17", author: "second@user.com" },
+        { date: "2018-03-15", hour: "16", author: "third@user.com" }
       ]
 
       const expectation = [
-        { year: "2018", month: "01", day: "08", hour: "16", minute: "44", second: "24", author: "first@user.com" },
-        { year: "2018", month: "03", day: "15", hour: "16", minute: "42", second: "00", author: "third@user.com" }
+        { date: "2018-01-08", hour: "16", author: "first@user.com" },
+        { date: "2018-03-15", hour: "16", author: "third@user.com" }
       ]
 
       const result = Filters.from("2018-01-01", commits)
@@ -65,16 +50,16 @@ describe("Filters", () => {
 
     it("should filter an array of commit data structures until a certain date", () => {
       const commits = [
-        { year: "2018", month: "01", day: "08", hour: "16", minute: "44", second: "24", author: "first@user.com" },
-        { year: "2017", month: "02", day: "23", hour: "17", minute: "03", second: "02", author: "second@user.com" },
-        { year: "2018", month: "03", day: "15", hour: "16", minute: "42", second: "00", author: "third@user.com" }
+        { date: "2018-01-08", hour: "16", author: "first@user.com" },
+        { date: "2017-02-23", hour: "17", author: "second@user.com" },
+        { date: "2018-03-15", hour: "16", author: "third@user.com" }
       ]
 
       const expectation = [
-        { year: "2017", month: "02", day: "23", hour: "17", minute: "03", second: "02", author: "second@user.com" }
+        { date: "2017-02-23", hour: "17", author: "second@user.com" }
       ]
 
-      const result = Filters.from("2017-12-31", commits)
+      const result = Filters.to("2017-12-31", commits)
       expect(result).to.deep.equal(expectation)
     })
 
