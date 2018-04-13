5f84f54359026cad3b898d9e0e1ecc3ea08071fc
ciamiz <luca@ramundo.info>
2018-04-13 20:53:08 +0200

    Update specs for Timesheet.compute method

--- specs/timesheet.js
+++ specs/timesheet.js
@@ -36,44 +36,42 @@ describe("Timesheet", () => {
 
       gitModule.__set__("log", () => gitLogOutput)
 
-      const expectation = [
-        { date: "2018-02-03", hours: [
-            { type: "PRESTART", hour: "12", commits: [] },
-            { type: "WORKED", hour: "13", commits: [
-              { date: "2018-02-03", hour: "13", author: "second@user.com" },
-              { date: "2018-02-03", hour: "13", author: "third@user.com" }
-            ] },
-            { type: "WORKED", hour: "14", commits: [
-              { date: "2018-02-03", hour: "14", author: "second@user.com" }
-            ] },
-            { type: "CONTINUITY", hour: "15", commits: [] },
-            { type: "WORKED", hour: "16", commits: [
-              { date: "2018-02-03", hour: "16", author: "third@user.com" }
-            ] },
-            { type: "WORKED", hour: "17", commits: [
-              { date: "2018-02-03", hour: "17", author: "third@user.com" }
-            ] },
-            { type: "CONTINUITY", hour: "18", commits: [] },
-            { type: "CONTINUITY", hour: "19", commits: [] },
-            { type: "WORKED", hour: "20", commits: [
-              { date: "2018-02-03", hour: "20", author: "third@user.com" }
-            ] }
-          ]
+      const expectation = {
+        "2018-02-03": {
+          "12": { type: "PRESTART", commits: [] },
+          "13": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "13", author: "second@user.com" },
+            { date: "2018-02-03", hour: "13", author: "third@user.com" }
+          ] },
+          "14": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "14", author: "second@user.com" }
+          ] },
+          "15": { type: "CONTINUITY", commits: [] },
+          "16": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "16", author: "third@user.com" }
+          ] },
+          "17": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "17", author: "third@user.com" }
+          ] },
+          "18": { type: "CONTINUITY", commits: [] },
+          "19": { type: "CONTINUITY", commits: [] },
+          "20": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "20", author: "third@user.com" }
+          ] }
         },
-        { date: "2018-02-05", hours: [
-            { type: "PRESTART", hour: "07", commits: [] },
-            { type: "WORKED", hour: "08", commits: [
-              { date: "2018-02-05", hour: "08", author: "second@user.com" }
-            ] },
-            { type: "WORKED", hour: "12", commits: [
-              { date: "2018-02-05", hour: "12", author: "third@user.com" }
-            ] },
-            { type: "WORKED", hour: "13", commits: [
-              { date: "2018-02-05", hour: "13", author: "third@user.com" }
-            ] }
-          ]
+        "2018-02-05": {
+          "07": { type: "PRESTART", commits: [] },
+          "08": { type: "WORKED", commits: [
+            { date: "2018-02-05", hour: "08", author: "second@user.com" }
+          ] },
+          "12": { type: "WORKED", commits: [
+            { date: "2018-02-05", hour: "12", author: "third@user.com" }
+          ] },
+          "13": { type: "WORKED", commits: [
+            { date: "2018-02-05", hour: "13", author: "third@user.com" }
+          ] }
         }
-      ]
+      }
 
       const result = compute(authors, from, to, continuity, prestart)
       expect(result).to.deep.equal(expectation)
