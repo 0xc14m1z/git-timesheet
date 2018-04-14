98b340db5d936fb869370715b26a8de74820bb16
Luca Ramundo <0xc14m1z@gmail.com>
2018-04-14 18:12:43 +0200

    Implement groupByDate method and relative specs

--- specs/timesheet.js
+++ specs/timesheet.js
@@ -178,4 +178,68 @@ describe("Timesheet", () => {
 
   })
 
+  describe("groupByDate", () => {
+
+    const groupByDate = currentModule.__get__("groupByDate")
+
+    it("should return an empty object if no commits are given", () => {
+      const commits = []
+
+      const expectation = {}
+
+      const result = groupByDate(commits)
+      expect(result).to.deep.equal(expectation)
+    })
+
+    it("should group commits by date", () => {
+      const commits = [
+        { date: "2018-02-01", hour: "09", author: "second@user.com" },
+        { date: "2018-02-03", hour: "12", author: "first@user.com" },
+        { date: "2018-02-03", hour: "13", author: "second@user.com" },
+        { date: "2018-02-03", hour: "13", author: "third@user.com" },
+        { date: "2018-02-03", hour: "14", author: "second@user.com" },
+        { date: "2018-02-03", hour: "16", author: "third@user.com" },
+        { date: "2018-02-03", hour: "17", author: "third@user.com" },
+        { date: "2018-02-03", hour: "20", author: "third@user.com" },
+        { date: "2018-02-05", hour: "08", author: "second@user.com" },
+        { date: "2018-02-05", hour: "12", author: "third@user.com" },
+        { date: "2018-02-05", hour: "13", author: "third@user.com" },
+        { date: "2018-02-05", hour: "17", author: "first@user.com" },
+        { date: "2018-02-11", hour: "12", author: "second@user.com" },
+        { date: "2018-02-12", hour: "12", author: "third@user.com" }
+      ]
+
+      const expectation = {
+        "2018-02-01": { hours: {}, commits: [
+          { date: "2018-02-01", hour: "09", author: "second@user.com" }
+        ] },
+        "2018-02-03": { hours: {}, commits: [
+          { date: "2018-02-03", hour: "12", author: "first@user.com" },
+          { date: "2018-02-03", hour: "13", author: "second@user.com" },
+          { date: "2018-02-03", hour: "13", author: "third@user.com" },
+          { date: "2018-02-03", hour: "14", author: "second@user.com" },
+          { date: "2018-02-03", hour: "16", author: "third@user.com" },
+          { date: "2018-02-03", hour: "17", author: "third@user.com" },
+          { date: "2018-02-03", hour: "20", author: "third@user.com" }
+        ] },
+        "2018-02-05": { hours: {}, commits: [
+          { date: "2018-02-05", hour: "08", author: "second@user.com" },
+          { date: "2018-02-05", hour: "12", author: "third@user.com" },
+          { date: "2018-02-05", hour: "13", author: "third@user.com" },
+          { date: "2018-02-05", hour: "17", author: "first@user.com" }
+        ] },
+        "2018-02-11": { hours: {}, commits: [
+          { date: "2018-02-11", hour: "12", author: "second@user.com" }
+        ] },
+        "2018-02-12": { hours: {}, commits: [
+          { date: "2018-02-12", hour: "12", author: "third@user.com" }
+        ] }
+      }
+
+      const result = groupByDate(commits)
+      expect(result).to.deep.equal(expectation)
+    })
+
+  })
+
 })
