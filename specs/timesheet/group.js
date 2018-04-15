2ccdf87516375e0fb9d8d23577544162840974ed
Luca Ramundo <0xc14m1z@gmail.com>
2018-04-15 11:19:07 +0200

    Refactor groupByDate method

--- specs/timesheet/group.js
+++ specs/timesheet/group.js
@@ -37,10 +37,10 @@ describe("Timesheet", () => {
       ]
 
       const expectation = {
-        "2018-02-01": { hours: {}, commits: [
+        "2018-02-01": [
           { date: "2018-02-01", hour: "09", author: "second@user.com" }
-        ] },
-        "2018-02-03": { hours: {}, commits: [
+        ],
+        "2018-02-03": [
           { date: "2018-02-03", hour: "12", author: "first@user.com" },
           { date: "2018-02-03", hour: "13", author: "second@user.com" },
           { date: "2018-02-03", hour: "13", author: "third@user.com" },
@@ -48,19 +48,19 @@ describe("Timesheet", () => {
           { date: "2018-02-03", hour: "16", author: "third@user.com" },
           { date: "2018-02-03", hour: "17", author: "third@user.com" },
           { date: "2018-02-03", hour: "20", author: "third@user.com" }
-        ] },
-        "2018-02-05": { hours: {}, commits: [
+        ],
+        "2018-02-05": [
           { date: "2018-02-05", hour: "08", author: "second@user.com" },
           { date: "2018-02-05", hour: "12", author: "third@user.com" },
           { date: "2018-02-05", hour: "13", author: "third@user.com" },
           { date: "2018-02-05", hour: "17", author: "first@user.com" }
-        ] },
-        "2018-02-11": { hours: {}, commits: [
+        ],
+        "2018-02-11":[
           { date: "2018-02-11", hour: "12", author: "second@user.com" }
-        ] },
-        "2018-02-12": { hours: {}, commits: [
+        ],
+        "2018-02-12": [
           { date: "2018-02-12", hour: "12", author: "third@user.com" }
-        ] }
+        ]
       }
 
       const result = groupByDate(commits)
