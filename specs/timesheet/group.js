fa4bc542c18b59d66036250d8d945f937aeb3481
ciamiz <luca@ramundo.info>
2018-04-16 16:54:09 +0200

    Refactor group method

--- specs/timesheet/group.js
+++ specs/timesheet/group.js
@@ -186,68 +186,58 @@ describe("Timesheet", () => {
         { date: "2018-02-12", hour: "12", author: "third@user.com" }
       ]
 
-      const expectation = [
-        { date: "2018-02-01",
-          hours: {
-            "09": { type: "WORKED", commits: [
-              { date: "2018-02-01", hour: "09", author: "second@user.com" }
-            ] }
-          }
+      const expectation = {
+        "2018-02-01": {
+          "09": { type: "WORKED", commits: [
+            { date: "2018-02-01", hour: "09", author: "second@user.com" }
+          ] }
         },
-        { date: "2018-02-03",
-          hours: {
-            "12": { type: "WORKED", commits: [
-              { date: "2018-02-03", hour: "12", author: "first@user.com" }
-            ] },
-            "13": { type: "WORKED", commits: [
-              { date: "2018-02-03", hour: "13", author: "second@user.com" },
-              { date: "2018-02-03", hour: "13", author: "third@user.com" }
-            ] },
-            "14": { type: "WORKED", commits: [
-              { date: "2018-02-03", hour: "14", author: "second@user.com" }
-            ] },
-            "16": { type: "WORKED", commits: [
-              { date: "2018-02-03", hour: "16", author: "third@user.com" }
-            ] },
-            "17": { type: "WORKED", commits: [
-              { date: "2018-02-03", hour: "17", author: "third@user.com" }
-            ] },
-            "20": { type: "WORKED", commits: [
-              { date: "2018-02-03", hour: "20", author: "third@user.com" }
-            ] }
-          }
+        "2018-02-03": {
+          "12": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "12", author: "first@user.com" }
+          ] },
+          "13": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "13", author: "second@user.com" },
+            { date: "2018-02-03", hour: "13", author: "third@user.com" }
+          ] },
+          "14": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "14", author: "second@user.com" }
+          ] },
+          "16": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "16", author: "third@user.com" }
+          ] },
+          "17": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "17", author: "third@user.com" }
+          ] },
+          "20": { type: "WORKED", commits: [
+            { date: "2018-02-03", hour: "20", author: "third@user.com" }
+          ] }
         },
-        { date: "2018-02-05",
-          hours: {
-            "08": { type: "WORKED", commits: [
-              { date: "2018-02-05", hour: "08", author: "second@user.com" }
-            ] },
-            "12": { type: "WORKED", commits: [
-              { date: "2018-02-05", hour: "12", author: "third@user.com" }
-            ] },
-            "13": { type: "WORKED", commits: [
-              { date: "2018-02-05", hour: "13", author: "third@user.com" }
-            ] },
-            "17": { type: "WORKED", commits: [
-              { date: "2018-02-05", hour: "17", author: "first@user.com" }
-            ] }
-          }
+        "2018-02-05": {
+          "08": { type: "WORKED", commits: [
+            { date: "2018-02-05", hour: "08", author: "second@user.com" }
+          ] },
+          "12": { type: "WORKED", commits: [
+            { date: "2018-02-05", hour: "12", author: "third@user.com" }
+          ] },
+          "13": { type: "WORKED", commits: [
+            { date: "2018-02-05", hour: "13", author: "third@user.com" }
+          ] },
+          "17": { type: "WORKED", commits: [
+            { date: "2018-02-05", hour: "17", author: "first@user.com" }
+          ] }
         },
-        { date: "2018-02-11",
-          hours: {
-            "12": { type: "WORKED", commits: [
-              { date: "2018-02-11", hour: "12", author: "second@user.com" }
-            ] }
-          }
+        "2018-02-11": {
+          "12": { type: "WORKED", commits: [
+            { date: "2018-02-11", hour: "12", author: "second@user.com" }
+          ] }
         },
-        { date: "2018-02-12",
-          hours: {
-            "12": { type: "WORKED", commits: [
-              { date: "2018-02-12", hour: "12", author: "third@user.com" }
-            ] }
-          }
+        "2018-02-12": {
+          "12": { type: "WORKED", commits: [
+            { date: "2018-02-12", hour: "12", author: "third@user.com" }
+          ] }
         }
-      ]
+      }
 
       const result = group(commits)
       expect(result).to.deep.equal(expectation)
