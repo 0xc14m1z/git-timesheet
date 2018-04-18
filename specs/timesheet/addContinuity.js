26cc95ef768c0c8b712570e55d2b4d89b570dd55
ciamiz <luca@ramundo.info>
2018-04-18 13:27:06 +0200

    Implement addContinuity method and update specs

--- specs/timesheet/addContinuity.js
+++ specs/timesheet/addContinuity.js
@@ -7,7 +7,7 @@ describe("Timesheet", () => {
   describe("addContinuity", () => {
 
     it("should return the same object if no continuty hours are given", () => {
-      const day = {
+      const days = {
         "2018-02-03": {
           "08": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "08", author: "second@user.com" }
@@ -34,7 +34,7 @@ describe("Timesheet", () => {
         },
         "2018-02-05": {
           "09": { type: "WORKED", commits: [
-            { date: "2018-02-05", hour: "08", author: "second@user.com" }
+            { date: "2018-02-05", hour: "09", author: "second@user.com" }
           ] },
           "12": { type: "WORKED", commits: [
             { date: "2018-02-05", hour: "12", author: "third@user.com" }
@@ -42,14 +42,14 @@ describe("Timesheet", () => {
         }
       }
 
-      const expectation = day
+      const expectation = days
 
-      const result = addContinuity(0)(day)
+      const result = addContinuity(0)(days)
       expect(result).to.deep.equal(expectation)
     })
 
     it("should add 1 hour of continuity between worked hours", () => {
-      const day = {
+      const days = {
         "2018-02-03": {
           "08": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "08", author: "second@user.com" }
@@ -76,7 +76,7 @@ describe("Timesheet", () => {
         },
         "2018-02-05": {
           "09": { type: "WORKED", commits: [
-            { date: "2018-02-05", hour: "08", author: "second@user.com" }
+            { date: "2018-02-05", hour: "09", author: "second@user.com" }
           ] },
           "12": { type: "WORKED", commits: [
             { date: "2018-02-05", hour: "12", author: "third@user.com" }
@@ -99,7 +99,7 @@ describe("Timesheet", () => {
           "14": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "14", author: "second@user.com" }
           ] },
-          "15": { type: "CONTINUITY", commits: [] },
+          "15": { type: "CONTINUITY" },
           "16": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "16", author: "third@user.com" }
           ] },
@@ -112,7 +112,7 @@ describe("Timesheet", () => {
         },
         "2018-02-05": {
           "09": { type: "WORKED", commits: [
-            { date: "2018-02-05", hour: "08", author: "second@user.com" }
+            { date: "2018-02-05", hour: "09", author: "second@user.com" }
           ] },
           "12": { type: "WORKED", commits: [
             { date: "2018-02-05", hour: "12", author: "third@user.com" }
@@ -120,12 +120,12 @@ describe("Timesheet", () => {
         }
       }
 
-      const result = addContinuity(0)(day)
+      const result = addContinuity(1)(days)
       expect(result).to.deep.equal(expectation)
     })
 
     it("should add 2 hours of continuity between worked hours", () => {
-      const day = {
+      const days = {
         "2018-02-03": {
           "08": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "08", author: "second@user.com" }
@@ -152,7 +152,7 @@ describe("Timesheet", () => {
         },
         "2018-02-05": {
           "09": { type: "WORKED", commits: [
-            { date: "2018-02-05", hour: "08", author: "second@user.com" }
+            { date: "2018-02-05", hour: "09", author: "second@user.com" }
           ] },
           "12": { type: "WORKED", commits: [
             { date: "2018-02-05", hour: "12", author: "third@user.com" }
@@ -175,32 +175,32 @@ describe("Timesheet", () => {
           "14": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "14", author: "second@user.com" }
           ] },
-          "15": { type: "CONTINUITY", commits: [] },
+          "15": { type: "CONTINUITY" },
           "16": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "16", author: "third@user.com" }
           ] },
           "17": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "17", author: "third@user.com" }
           ] },
-          "18": { type: "CONTINUITY", commits: [] },
-          "19": { type: "CONTINUITY", commits: [] },
+          "18": { type: "CONTINUITY" },
+          "19": { type: "CONTINUITY" },
           "20": { type: "WORKED", commits: [
             { date: "2018-02-03", hour: "20", author: "third@user.com" }
           ] }
         },
         "2018-02-05": {
           "09": { type: "WORKED", commits: [
-            { date: "2018-02-05", hour: "08", author: "second@user.com" }
+            { date: "2018-02-05", hour: "09", author: "second@user.com" }
           ] },
-          "10": { type: "CONTINUITY", commits: [] },
-          "11": { type: "CONTINUITY", commits: [] },
+          "10": { type: "CONTINUITY" },
+          "11": { type: "CONTINUITY" },
           "12": { type: "WORKED", commits: [
             { date: "2018-02-05", hour: "12", author: "third@user.com" }
           ] }
         }
       }
 
-      const result = addContinuity(2)(day)
+      const result = addContinuity(2)(days)
       expect(result).to.deep.equal(expectation)
     })
 
